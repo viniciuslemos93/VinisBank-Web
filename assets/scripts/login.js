c69ef8e21295afbc1c55af7ec40136eb9168c72f
Vinicius Lemos <vinycius1993@gmail.com>
2023-01-31 14:05:40 -0300

    criando a função de depositar

--- assets/scripts/login.js
+++ assets/scripts/login.js
@@ -1,3 +1,11 @@
-function bemVindo () {
-    login.loginBD = sessionStorage.getItem("login")
+document.getElementById('deposito').addEventListener('click', depositar);
+var saldo = document.getElementById('saldo').innerText
+parseFloat(saldo)
+function depositar() {    
+    var deposito = prompt("Valor do Depósito");
+    
+    saldo = saldo + deposito
+    //saldo = parseFloat(saldo + deposito)
+    //saldo = parseFloat((saldo) + (deposito))
+    console.log(saldo)    
 }
