317b8dfc8cd7cca9a9a990f6d3406736922381bf
Vinicius Lemos <vinycius1993@gmail.com>
2023-01-31 19:21:13 -0300

    Ajustado o bem vindo para receber o login

--- assets/scripts/login.js
+++ assets/scripts/login.js
@@ -1,7 +1,14 @@
+var login = sessionStorage.getItem("login")
+
+var bemVindo = document.getElementById('bem-vindo')
+bemVindo.innerHTML=`Bem vindo(a) ${login}`
+
+
+
 document.getElementById('deposito').addEventListener('click', depositar);
 var saldo = document.getElementById('saldo').innerText
 parseFloat(saldo)
-function depositar() {    
+function depositar() {
     var deposito = prompt("Valor do Depósito");
     
     saldo = saldo + deposito
